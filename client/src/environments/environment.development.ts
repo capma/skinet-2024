fa3bece5f2e857f6c612b01e13d4815005d26c72
tpham1 <tri.pham@agdata.com>
2024-09-09 23:22:19 -0400

    End of section 18

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51PwChzISdJNGXTUI5fHEKe81Tn4nBCrz664d39z15fSeAFRG2zI6k0dRxTliBAkIJxw5DaMIXOKhXxM9ZMjITBYX00hiCDTxEM'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51PwChzISdJNGXTUI5fHEKe81Tn4nBCrz664d39z15fSeAFRG2zI6k0dRxTliBAkIJxw5DaMIXOKhXxM9ZMjITBYX00hiCDTxEM'
 };
